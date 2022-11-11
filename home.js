5463ed498bb3d62aaf175bfbe73b174f9242ccdf
pilirolon <prolon@udesa.edu.ar>
2022-11-11 11:46:43 -0300

    nada

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 
 let ruta = "https://api.themoviedb.org/3/movie/550?api_key=45d43a6901861343cdb188d4f3bafd7c'"
 
-let lupita = document.querySelector('.lupita')
+lupita.addEventListener()
 
 lupita.addEventListener('click', function(){
     window.open(this) // aca va el link de lo q quiero buscar
