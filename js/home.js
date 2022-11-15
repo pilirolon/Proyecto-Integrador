149342b953910cadb9e815f6ea9ad2bdbf03f3f1
pilirolon <prolon@udesa.edu.ar>
2022-11-15 16:36:19 -0300

    comente un par de cosas

--- js/home.js
+++ js/home.js
@@ -1,21 +1,20 @@
-let lupita = document.querySelector('.lupita')
-let busqueda = 
+let ruta = "https://api.themoviedb.org/3/movie/popular?api_key=45d43a6901861343cdb188d4f3bafd7c&language=en-US&page=1"
 
-// capturar la query de la url
-// let queryString = location.search;
-// let queryStringObj = new URLSearchParams(queryString);
-// queryStringObj.get('buscador');	// alien
 
-lupita.addEventlistener('click', function(){
-    window.open
+
+
+let peliculas=document.querySelector(".pelicula-principal")
+
+fetch(ruta) 
+    for (let i = 0; i < 6 ; i++){    
+    }
+
+	then(function(response){
+	return response.json();
 })
-
-lupita.addEventListener('click', function(){
-    window.open(this) // aca va el link de lo q quiero buscar
-});
-
-lupita.addEventListener('mouseover', function(){
-    window.open(this) // aca va el link de lo q quiero buscar
-});
-
-let ruta = "https://api.themoviedb.org/3/movie/550?api_key=45d43a6901861343cdb188d4f3bafd7c'"
+	.then(function(data){
+	console.log(data);
+})
+	.catch(function(error){
+	console.log('El error es: ' + error);
+})
